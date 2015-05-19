ba57f6f1f3886c1cccfa12b247a53463ba5b554a
cano_c <cano_c@epitech.eu>
2015-05-19 19:19:01 +0000

    fix pipe priority over

--- src/parse_cmd.c
+++ src/parse_cmd.c
@@ -5,7 +5,7 @@
 ** Login   <cano_c@epitech.net>
 ** 
 ** Started on  Tue May  5 11:49:27 2015 
-** Last update Tue May  5 11:49:34 2015 
+** Last update Tue May 19 19:12:09 2015 
 */
 #include <parser.h>
 
@@ -31,4 +31,27 @@ t_ast		*parse_cmd(t_ast **ast, t_lex **lexem)
       return (c_ast);
     }
   return (parse_err(ast, lexem));
+}
+
+t_ast		*parse_pipeline(t_ast **ast, t_lex **lexem)
+{
+  t_lex		*next;
+  t_ast		*i_ast;
+  t_ast		*tmp;
+
+  if (DEBUG_PARSER)
+    my_putstr("parser -> PIPELINE\n");
+  if (!(i_ast = parse_cmd(ast, lexem)))
+    return (NULL);
+  while ((next = lexer_next_token(*lexem)) && next->type == LX_BAR)
+    {
+      tmp = i_ast;
+      if (!(i_ast = make_binary(ast, &next)))
+	return (NULL);
+      i_ast->left = tmp;
+      *lexem = next->next;
+      if (!(i_ast->right = parse_cmd(ast, lexem)))
+	return (NULL);
+    }
+  return (i_ast);
 }
--- src/parse_instr.c
+++ src/parse_instr.c
@@ -5,7 +5,7 @@
 ** Login   <cano_c@epitech.net>
 ** 
 ** Started on  Tue May  5 11:52:28 2015 
-** Last update Sun May 10 02:31:17 2015 
+** Last update Tue May 19 19:13:14 2015 
 */
 #include <parser.h>
 
@@ -19,21 +19,21 @@ t_ast		*parse_instr(t_ast **ast, t_lex **lexem)
     my_putstr("parser -> INSTR\n");
   if ((next = lexer_next_token(*lexem)) && next->type != LX_EOL)
     {
-      if (!(i_ast = parse_cmd(ast, lexem)))
+      if (!(i_ast = parse_pipeline(ast, lexem)))
 	return (NULL);
       while ((next = lexer_next_token(*lexem))
-	     && next->type >= LX_BAR && next->type <= LX_OR)
+	     && next->type >= LX_AND && next->type <= LX_OR)
 	{
 	  tmp = i_ast;
 	  if (!(i_ast = make_binary(ast, &next)))
 	    return (NULL);
 	  i_ast->left = tmp;
 	  *lexem = next->next;
-	  if (!(i_ast->right = parse_cmd(ast, lexem)))
+	  if (!(i_ast->right = parse_pipeline(ast, lexem)))
 	    return (NULL);
 	}
     }
   else if ((*lexem)->type != LX_EOL)
-    return (parse_cmd(ast, lexem));
+    return (parse_pipeline(ast, lexem));
   return (i_ast);
 }
