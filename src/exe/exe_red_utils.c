c03950e40f4649c0c1ca5b32c570b657ac026224
Paul Kerebel <paul.kerebel@epitech.eu>
2015-05-23 23:32:34 +0200

    Update

--- src/exe/exe_red_utils.c
+++ src/exe/exe_red_utils.c
@@ -10,46 +10,27 @@
 
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <get_next_line.h>
 #include <mysh.h>
 
-int             open_failure(char *file, int flags)
+void    check_dash_line(char *s, char *file, t_list *list)
 {
-  struct stat   s_stat;
-
-  if (access(file, F_OK))
-    {
-      if (flags & O_TRUNC)
-	fprintf(stderr, "42sh: %s: cannot create file\n", file);
-      else
-	fprintf(stderr, "42sh: %s: no such file or directory\n", file);
-    }
-  else if ((flags & O_WRONLY && access(file, W_OK))
-	   || (!flags && access(file, R_OK)))
-    fprintf(stderr, "42sh: %s: permission denied\n", file);
-  else if (flags && !(stat(file, &s_stat)) && S_ISDIR(s_stat.st_mode))
-    fprintf(stderr, "42sh: %s: is a directory\n", file);
-  else
-    fprintf(stderr, "42sh: %s: undefined error at open\n", file);
-  return (-1);
+  if (s != NULL &&
+      my_strncmp(s, file, my_strlen(s)) != 0)
+    list_add(list, s, "");
+  if (s == NULL)
+    fprintf(stderr, "42sh: warning: delimited by end-of-file (wanted `%s')\n"
+	    , file);
 }
 
-int     swap_red(t_ast *ast, int red_fd[2])
-{
-  ast->content.red[0] = ast->content.red[1];
-  ast->content.red[1] = ast->content.red[2];
-  ast->content.red[2] = 0;
-  red_fd[1] = 2;
-  return (0);
-}
-
-void     exec_parallel(t_ast *ast, t_mysh *sh, int fd[2])
+void     exec_parallel(t_ast *ast, t_mysh *sh, int fd[2], t_job *job)
 {
   int   s;
 
   close(fd[1]);
   s = dup(0);
   dup2(fd[0], 0);
-  sh->status = sh->exe_ft[ast->right->type](ast->right, sh);
+  sh->status = sh->exe_ft[ast->right->type](ast->right, sh, job);
   dup2(0, fd[0]);
   dup2(s, 0);
 }
@@ -69,4 +50,41 @@ void             exec_double_dash_left(t_list *list, int fd[2])
     }
   dup2(1, fd[1]);
   exit(0);
+}
+
+int             do_double_red(t_ast *ast, t_mysh *sh, char *s, t_job *job)
+{
+  int           pid;
+  int           fd[2];
+  t_list        *list;
+
+  list = list_create();
+  can_set(sh->tsave);
+  while (s != NULL && my_strcmp(s, ast->left->content.file) != 0)
+    {
+      write(0, "> ", 2);
+      s = get_next_line(0);
+      check_dash_line(s, ast->left->content.file, list);
+    }
+  pipe(fd);
+  if ((pid = fork()) < 0)
+    return (-1);
+  if (pid == 0)
+    exec_double_dash_left(list, fd);
+  else if (pid > 0)
+    exec_parallel(ast, sh, fd, job);
+  can_on(&sh->term);
+  wait(&pid);
+  return (0);
+}
+
+int     dash_left_check(int red_fd[2], int flags, t_ast *ast, t_mysh *sh, t_job *job)
+{
+  int   ret;
+
+  if (ast->content.red[0] == '<' && ast->content.red[1])
+    ret = do_double_red(ast, sh, "", job);
+  else
+    ret = do_red(red_fd, flags, ast, sh, job);
+  return (ret);
 }
